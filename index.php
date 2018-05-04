ceb41802fcbbe85de0e8128e3190048d8b147c3e
Daniel Lim <dan@daniell.im>
2018-05-04 16:52:13 +0800

    Fix homepage carousel appearance

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 ?>
 
 <!-- Add the slick-theme.css if you want default styling -->
-<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick/slick/slick.css"/>
+<link rel="stylesheet" type="text/css" href="scripts/slick/slick.css"/>
 <!-- Add the slick-theme.css if you want default styling -->
-<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/gh/kenwheeler/slick/slick/slick-theme.css"/>
+<link rel="stylesheet" type="text/css" href="scripts/slick/slick-theme.css"/>
 </head>
 
 				
@@ -42,7 +42,7 @@
 	$outputposters = "";
 	
 	foreach ($recentquery_array as $key=>$value) {
-		$outputposters = $outputposters . "<div><img class='index-poster' src=https://image.tmdb.org/t/p/w500/$recentquery_array[$key]></div>";
+		$outputposters = $outputposters . "<div><img class='poster-slides' src='https://image.tmdb.org/t/p/w300/$recentquery_array[$key]'></div>";
 	}
 	
 ?>
@@ -57,10 +57,10 @@
 </section>
 
 <!-- User movies scroller -->
-<div class="poster-carousel-formatting">
-  <h1 class="col-xs-10 col-sm-10 col-centered index-subheader">What others have recently added...</h1>
-  <div class="slick-list draggable center-posters">
-  	<div class="poster-carousel slick-track">
+<div style="padding-bottom: 328px;">
+  <h1 class="col-xs-10 col-sm-10 col-centered index-subheader">What others have recently added to their lists...</h1>
+  <div>
+  	<div class="poster-carousel centering-carousel">
   	  <?php echo $outputposters; ?>
     </div>
   </div>
@@ -77,50 +77,21 @@
 
 <!-- Scripts for Slick carousel-->
 
-<script type="text/javascript" src="//cdn.jsdelivr.net/gh/kenwheeler/slick/slick/slick.min.js"></script>
+<script type="text/javascript" src="scripts/slick/slick.min.js"></script>
 
 <script>
 	$(document).ready(function(){
-	  $('.poster-carousel').slick({
-	  dots: true,
-	  infinite: true,
-	  speed: 300,
-	  slidesToShow: 4,
-	  slidesToScroll: 3,
-	  autoplay: true,
-      autoplaySpeed: 2500,
-	  centerMode: true,
-      centerPadding: '40px',
-	  arrows: true,
-	  responsive: [
-		{
-		  breakpoint: 1024,
-		  settings: {
-			slidesToShow: 3,
-			slidesToScroll: 3,
+		$('.poster-carousel').slick({
+			speed: 500,
+			slidesToScroll: 1,
 			infinite: true,
-			dots: true
-		  }
-		},
-		{
-		  breakpoint: 600,
-		  settings: {
-			slidesToShow: 2,
-			slidesToScroll: 2
-		  }
-		},
-		{
-		  breakpoint: 480,
-		  settings: {
-			slidesToShow: 1,
-			slidesToScroll: 1
-		  }
-		}
-		// You can unslick at a given breakpoint now by adding:
-		// settings: "unslick"
-		// instead of a settings object
-	  ]
-	  });
+			autoplay: true,
+			autoplaySpeed: 2500,
+			slidesToShow: 4,
+			arrows: true,
+			centerMode: true,
+			centerPadding: '120px'
+		});
 	});
 </script>
 
